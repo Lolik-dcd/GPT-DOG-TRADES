1baa3111bf16acfd6b914db81cf1337bb1e60d59
Lolik-dcd <kolamuzyka77@gmail.com>
2025-08-19 20:56:16 +0300

    Delete config.py

--- config.py
+++
@@ -1,3 +0,0 @@
-TOKEN = "7895470939:AAEFSLYDEbfwWOY3P_j-vmYSsO1LNL8Z5Fk"
-ADMIN_ID = 7351552004
-REF_LINK = "https://u3.shortink.io/register?utm_campaign=815121&utm_source=affiliate&utm_medium=sr&a=kBQn8bFXGHmrzs&ac=dogsmoktr"
